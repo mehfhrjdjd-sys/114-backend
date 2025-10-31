7a6af166e3728b5b1a3fe30c59cf6772a45fe3a1
10_31 <mehfhrjdjd@gmail.com>
2025-10-31 16:05:34 +0800

    1031

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Item(BaseModel):
      )
      price: float = Field(gt = 0 ,description = "The price must be greater than zero")
      tax: float | None =None
-     tags : list = [str] = []
+     tags : list = [str] == []
 
 
 app = FastAPI()
